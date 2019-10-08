ef37a8bd57c5db602cd7fed2403a017426d34be2
yls <1329926763@qq.com>
2019-10-08 18:56:21 +0800

    1

--- book/book/pipelines.py
+++ book/book/pipelines.py
@@ -4,13 +4,23 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import json, os
 
 
 class BookPipeline(object):
     i = 1
 
     def process_item(self, item, spider):
-        print(item)
-        self.i+=1
-        print(self.i)
+        base_dir = os.getcwd()
+
+        filename = base_dir + '/news.json'
+        # 打开json文件，向里面以dumps的方式吸入数据
+        # 注意需要有一个参数ensure_ascii=False ，不然数据会直接为utf编码的方式存入比如
+        # :“/xe15”
+
+        with open(filename, 'a',encoding='utf-8') as f:
+            line = json.dumps(dict(item), ensure_ascii=False) + '\n'
+
+            f.write(line)
+
         return item
